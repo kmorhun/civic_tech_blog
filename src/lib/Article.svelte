0ad26d6d3d5b0623470e9f9e8682f81146bababd
Kateryna Morhun <kmorhun@gmail.com>
2026-09-14 20:02:47 -0400

    add week 1 reflection

--- src/lib/Article.svelte
+++ src/lib/Article.svelte
@@ -7,18 +7,16 @@
       <p class="text-uppercase text-muted">Week {week}</p>
       <h3>{title}</h3>
       <p class="text-muted">
-        Reading: {@html reading}
+        Reading: <a href={article_url}>{@html reading}</a>
       </p>
       <img
         src={imgsrc}
         class="img-fluid rounded mb-3"
+        style="max-width: 50%;"
         alt={imgalt}
       >
-
-      <a href={reflection_url} class="btn btn-primary">View Reflection</a>
-
-      <p>
-        <a href={article_url}>Link to the reading or project</a>
-      </p>
+      <div>
+        <a href={reflection_url} class="btn btn-primary">View Reflection</a>
+      </div>
     </article>
 </div>
